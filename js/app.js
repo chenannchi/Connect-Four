8e4c6955632d009452de736c0f60926cf11ea969
chenannchi <anchichenusa@gmail.com>
2022-09-10 13:10:49 -0400

    add getwinner function

--- js/app.js
+++ js/app.js
@@ -6,12 +6,11 @@ const mang = '<img src="./assets/mang.png">'
 const rj = '<img src="./assets/rj.png">'
 const shooky = '<img src="./assets/shooky.png">'
 
+const rows = 6, cols = 7
 
 /*---------------------------- Variables (state) ----------------------------*/
 let board, turn, winner, themeColor
 let playing = 0
-
-
 
 /*------------------------ Cached Element References ------------------------*/
 let boardEl = document.querySelector("#game-board")
@@ -31,84 +30,101 @@ let audioBtn = document.getElementById("audio")
 // console.log(homeBtn)
 
 /*----------------------------- Event Listeners -----------------------------*/
-document.getElementById("timer").addEventListener("click",function(){
+document.getElementById("timer").addEventListener("click", function () {
   console.log("timer clicked")
 })
 
-audioBtn.addEventListener("click",function(evt){
+audioBtn.addEventListener("click", function (evt) {
   // console.log("audio clicked")
   // console.log(playing)
-  if(!playing){
+  if (!playing) {
     console.log("start-playing")
     music.volume = 0.1
     music.play()
-    audioBtn.setAttribute("src","/assets/audio.png")
+    audioBtn.setAttribute("src", "/assets/audio.png")
     playing = 1
-  }else{
+  } else {
     console.log("stop playing")
     music.pause()
-    audioBtn.setAttribute("src","/assets/no-audio.png")
+    audioBtn.setAttribute("src", "/assets/no-audio.png")
     playing = 0
   }
 })
 
-document.getElementById("paint-palette").addEventListener("click",function(){
+document.getElementById("paint-palette").addEventListener("click", function () {
   console.log("paint palette clicked")
 })
 
 boardEl.addEventListener("click", handleClick)
 
+restartBtn.addEventListener("click", init)
 
 /*-------------------------------- Functions --------------------------------*/
 init()
 
-function init(){
+function init() {
   board = new Array(6).fill(null)
-  for(let i = 0; i < 6;i++){
+  for (let i = 0; i < 6; i++) {
     board[i] = new Array(7).fill(null)
   }
   // console.log(board)
+
+  // for(let i = 0; i < rows;i++){
+  //   for(let j = 0; j < cols;j++){
+  //     squareEls[i][j].textContent = ""
+  //   }
+  // }
+
+  // board.forEach(ele, rowIdx=>{
+  //   ele.forEach(sq, colIdx=>{
+  //     squareEls[rowIdx][colIdx].textContent = ""
+  //   })
+  // })
+
   turn = 1
   winner = null
   render()
 }
 
-function render(){
-  board.forEach(function(row,rowIdx){
-    row.forEach(function(square,colIdx){
+function render() {
+  board.forEach(function (row, rowIdx) {
+    row.forEach(function (square, colIdx) {
       // console.log(rowIdx)
       // console.log(colIdx)
-      let idxOfSq = rowIdx*7+colIdx
-      if(board[rowIdx][colIdx] === 1){
-        console.log(squareEls[idxOfSq])
-        squareEls[idxOfSq].setAttribute("class","player1")
+      let idxOfSq = rowIdx * 7 + colIdx
+      if (board[rowIdx][colIdx] === 1) {
+        // console.log(squareEls[idxOfSq])
+        // Restart要將圖片清空
+        squareEls[idxOfSq].setAttribute("class", "player1")
         squareEls[idxOfSq].innerHTML = chimmy
-      }else if (board[rowIdx][colIdx] === -1){
-        squareEls[idxOfSq].setAttribute("class","player2")
+      } else if (board[rowIdx][colIdx] === -1) {
+        squ
+        squareEls[idxOfSq].setAttribute("class", "player2")
         squareEls[idxOfSq].innerHTML = shooky
-      }else{
-        squareEls[idxOfSq].setAttribute("class","blank")
+      } else {
+        squareEls[idxOfSq].setAttribute("class", "blank")
       }
     })
   });
 
-  if(winner === null){
+  if (winner === null) {
     hintMsg.textContent = `${turn === 1 ? "Player1" : "Player2"}, it's your turn!`
-  }else if (winner === "T"){
+  } else if (winner === "T") {
     hintMsg.textContent = "It's a tie!"
-  }else{
-    hintMsg.textContent = `${winner === 1 ? "Player1":"Player2"} win!!!!!`
+  } else {
+    hintMsg.textContent = `${winner === 1 ? "Player1" : "Player2"} win!!!!!`
   }
 }
 
-function handleClick(evt){
-  const sqRowIdx = parseInt(evt.target.id[evt.target.id.length-2])
+function handleClick(evt) {
+  const sqRowIdx = parseInt(evt.target.id[evt.target.id.length - 2])
+  // console.log("clicked sq row:", sqRowIdx)
+  const sqColIdx = parseInt(evt.target.id[evt.target.id.length - 1])
+  // console.log("clicked sq col:", sqColIdx)
 
-  const sqColIdx = parseInt(evt.target.id[evt.target.id.length-1])
-
-  if(board[sqRowIdx][sqColIdx]){
-    return 
-  }else if (winner!==null){
+  if (board[sqRowIdx][sqColIdx]) {
+    return
+  } else if (winner !== null) {
     return
   }
 
@@ -118,4 +134,64 @@ function handleClick(evt){
   render()
 }
 
+function getWinner() {
+  // horizontal
+  let total
+  for (let i = 0; i < rows; i++) {
+    total = 0
+    for (let j = 0; j < cols - 3; j++) {
+      total = board[i][j] + board[i][j + 1] + board[i][j + 2] + board[i][j + 3]
+      if (Math.abs(total) === 4) {
+        return board[i][j]
+      }
+    }
+  }
 
+  // vertical 
+  for (let i = 0; i < cols; i++) {
+    total = 0
+    for (let j = 0; j < rows - 3; j++) {
+      total = board[j][i] + board[j + 1][i] + board[j + 2][i] + board[j + 3][i]
+      if (Math.abs(total) === 4) {
+        return board[j][i]
+      }
+    }
+
+  }
+
+  // ascending
+  for (let i = 3; i < rows; i++) {
+    total = 0
+    for (let j = 0; j < cols - 3; j++) {
+      total = board[i][j] + board[i - 1][j + 1] + board[i - 2][j + 2] + board[i - 3][j + 3]
+      if (Math.abs(total) === 4) {
+        return board[i][j]
+      }
+    }
+  }
+
+  // descending
+  for (let i = 3; i < rows; i++) {
+    total = 0
+    for (let j = 3; j < cols; j++) {
+      total = board[i][j] + board[i - 1][j - 1] + board[i - 2][j - 2] + board[i - 3][j - 3]
+      if (Math.abs(total) === 4) {
+        return board[i][j]
+      }
+    }
+  }
+
+  let emptyR = 0
+  board.forEach(function(row){
+    if (!row.includes(null)) {
+      emptyR+=1
+      if (emptyR === rows){
+        winner = "T"
+      }
+    } else {
+      winner = null
+    }
+  })
+
+  return winner
+}
